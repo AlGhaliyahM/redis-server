29954b6dd8297814bde5fa62905c5a50e2673255
AlGhaliyahM <439201044@student.ksu.edu.sa>
2024-02-06 22:48:55 +0300

    setup RESP file for serializing and deserializing

---
+++ dist/resp.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Value = /** @class */ (function () {
+    function Value(typ, str, num, bulk, array) {
+        this.typ = "";
+        this.str = "";
+        this.num = 0;
+        this.bulk = "";
+        this.array = [];
+    }
+    return Value;
+}());
+var Resp = /** @class */ (function () {
+    function Resp() {
+    }
+    return Resp;
+}());
+function readLine() {
+    //logic
+}
+function readInteger() {
+    //logic
+}
+// const reader = readline.createInterface({
+//   input: Buffer.from(input),
+// });
+// let b = reader.readByte();
+// let size = reader.readByte();
+// reader.on("line", (line: string) => {
+//   // Your logic for processing each line goes here
+//   console.log(line);
+// });
--- src/resp.ts
+++ src/resp.ts
@@ -1,2 +1,51 @@
 // RESP serializing and deserializing
-import { SimpleString, Error, Integer, BulkString, Array } from "./enum";
+import * as readline from "readline";
+import * as fs from "fs";
+
+class Value {
+  typ: string;
+  str: string;
+  num: number;
+  bulk: string;
+  array: Value[];
+
+  constructor(
+    typ: string,
+    str: string,
+    num: number,
+    bulk: string,
+    array: Value[]
+  ) {
+    this.typ = "";
+    this.str = "";
+    this.num = 0;
+    this.bulk = "";
+    this.array = [];
+  }
+}
+
+class Resp {
+  //   reader: ReadableStream<Uint8Array>;
+  //   constructor(rd: Reader) {
+  //      this.reader = createBufferedReader(rd);
+  //   }
+}
+
+function readLine() {
+  //logic
+}
+
+function readInteger() {
+  //logic
+}
+
+// const reader = readline.createInterface({
+//   input: Buffer.from(input),
+// });
+// let b = reader.readByte();
+// let size = reader.readByte();
+
+// reader.on("line", (line: string) => {
+//   // Your logic for processing each line goes here
+//   console.log(line);
+// });
